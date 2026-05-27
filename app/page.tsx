af46ad3c841fab2dbfc0b657f9ded8e8d911e991
sugarban-p <271395910+sugarban-p@users.noreply.github.com>
2026-05-27 16:31:44 +0800

    計數器練習 - React 狀態宣告 (useState)

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ export default function HomePage() {
       <div>Hello JK</div>
       <a href="/about">關於網站</a>
       <a href="/products">商品總覽</a>
+      <a href="/counter">計數器練習</a>
     </>
   );
 }
