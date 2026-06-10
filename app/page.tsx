8b354a5cec9d7e0e1eb14c45696ab9fe2edede29
sugarban-p <271395910+sugarban-p@users.noreply.github.com>
2026-06-11 01:31:09 +0800

    --edit 調整專案架構

--- app/page.tsx
+++ app/page.tsx
@@ -2,76 +2,84 @@
 // 適用於 互動 UI、事件處理 等頁面
 'use client';
 
+import Link from 'next/link';
+
 export default function HomePage() {
   return (
     <>
       <div>
         <h1>首頁</h1>
         <div>Hello JK</div>
-        <a href="/about">關於網站 ( /about )</a>
+        <Link href="/about">關於網站 ( /about )</Link>
         <br />
-        <a href="/products">商品總覽 ( /products )</a>
+        <Link href="/products">商品總覽 ( /products )</Link>
         <br />
-        <a href="/counter-v1">計數器練習v1 ( /counter-v1 )</a>
+        <Link href="/counter-v1">計數器練習v1 ( /counter-v1 )</Link>
         <hr />
         <h4>CH3</h4>
-        <a href="/ch3-jsx/render">JSX 渲染範例 ( /ch3-jsx/render )</a>
+        <Link href="/ch3-jsx/render">JSX 渲染範例 ( /ch3-jsx/render )</Link>
         <br />
-        <a href="/ch3-jsx/attributes">JSX 標籤屬性 ( /ch3-jsx/attributes )</a>
+        <Link href="/ch3-jsx/attributes">
+          JSX 標籤屬性 ( /ch3-jsx/attributes )
+        </Link>
         <br />
-        <a href="/ch3-jsx/basic-synax">JSX 基本語法 ( /ch3-jsx/basic-synax )</a>
+        <Link href="/ch3-jsx/basic-synax">
+          JSX 基本語法 ( /ch3-jsx/basic-synax )
+        </Link>
         <hr />
         <h4>CH4</h4>
-        <a href="/ch4-state/cart-v1">
+        <Link href="/ch4-state/cart-v1">
           更新狀態練習 - 購物車v1 ( /ch4-state/cart-v1 )
-        </a>
+        </Link>
         <br />
-        <a href="/ch4-state/todo-v1">
+        <Link href="/ch4-state/todo-v1">
           更新狀態練習 - 待辦事項v1 ( /ch4-state/todo-v1 )
-        </a>
+        </Link>
         <br />
-        <a href="/ch4-state/immer">immer使用範例 ( /ch4-state/immer )</a>
+        <Link href="/ch4-state/immer">immer使用範例 ( /ch4-state/immer )</Link>
         <hr />
         <h4>CH5</h4>
-        <a href="/ch5-props/parent-child">
+        <Link href="/ch5-props/parent-child">
           父/子層關係 ( /ch5-props/parent-child )
-        </a>
+        </Link>
         <br />
-        <a href="/ch5-props/children">
+        <Link href="/ch5-props/children">
           props.children 範例 ( /ch5-props/children )
-        </a>
+        </Link>
         <br />
-        <a href="/ch5-props/todo-v2">
+        <Link href="/ch5-props/todo-v2">
           屬性練習 - 待辦事項v2 ( /ch5-props/todo-v2 )
-        </a>
+        </Link>
         <br />
-        <a href="/ch5-props/context-bg">
+        <Link href="/ch5-props/context-bg">
           context 練習 - 切換背景 ( /ch5-props/context-bg )
-        </a>
+        </Link>
         <br />
-        <a href="/ch5-props/cart-v2">
+        <Link href="/ch5-props/cart-v2">
           context 練習 - 購物車v2 ( /ch5-props/cart-v2 )
-        </a>
+        </Link>
         <br />
-        <a href="/ch5-props/user/login">
+        <Link href="/ch5-props/user/login">
           context 練習 - 會員登入 ( /ch5-props/user/login )
-        </a>
+        </Link>
         <br />
         <h4>CH6</h4>
-        <a href="/ch6-form/controlled">可控表單元件 ( /ch6-form/controlled )</a>
+        <Link href="/ch6-form/controlled">
+          可控表單元件 ( /ch6-form/controlled )
+        </Link>
         <br />
-        <a href="/ch6-form/uncontrolled">
+        <Link href="/ch6-form/uncontrolled">
           不可控表單元件 ( /ch6-form/uncontrolled )
-        </a>
+        </Link>
         <br />
         <h4>CH7</h4>
-        <a href="/ch7-use-effect/pattern">
+        <Link href="/ch7-use-effect/pattern">
           useEffect() 應用 ( /ch7-use-effect/pattern )
-        </a>
+        </Link>
         <br />
-        <a href="/ch7-use-effect/product-v2">
+        <Link href="/ch7-use-effect/product-v2">
           useEffect() 練習 - 商品列表v2 ( /ch7-use-effect/product-v2 )
-        </a>
+        </Link>
         <br />
       </div>
     </>
---
+++ types/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  stock: number;
+  image_url: string;
+  created_at: string;
+  updated_at: string;
+}
